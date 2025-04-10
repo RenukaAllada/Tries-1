13cb536da58d0334f6ddd5f3b186f2e4f85058cf
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-04-10 17:31:08 -0500

    Completed 2 exercises

--- Sample.java
+++ Sample.java
@@ -1,5 +1,5 @@
 class Sample{
-    /***************PROBLEM-1*******************/
+    /***************PROBLEM-2*******************/
     //TC:0(n*k)
     //SC:0(n)
     class Solution {
@@ -62,5 +62,70 @@ class Sample{
         }
     }
 
-    /***********************PROBLEM-2*******************/
+    /***********************PROBLEM-1*******************/
+
+    class Trie {
+        class TrieNode{
+            TrieNode[] children;
+            boolean isEnd;
+            public TrieNode(){
+                children=new TrieNode[26];
+                isEnd=false;
+            }
+        }
+        TrieNode root;
+        public Trie() {
+            root=new TrieNode();
+        }
+        //TC:0(N)
+        //SC:0(1)
+        public void insert(String word) {
+            TrieNode curr=root;
+            for(int i=0;i<word.length();i++){
+                char c=word.charAt(i);
+                if(curr.children[c-'a']==null){
+                    curr.children[c-'a']=new TrieNode();
+                }
+                curr=curr.children[c-'a'];
+            }
+            curr.isEnd=true;
+        }
+        //TC:0(N)
+        //SC:0(1)
+        public boolean search(String word) {
+            TrieNode curr=root;
+            for(int i=0;i<word.length();i++){
+                char c=word.charAt(i);
+                if(curr.children[c-'a']==null){
+                    return false;
+                }
+                curr=curr.children[c-'a'];
+            }
+            return curr.isEnd;
+        }
+        //TC:0(N)
+        //SC:0(1)
+        public boolean startsWith(String prefix) {
+            TrieNode curr=root;
+            for(int i=0;i<prefix.length();i++){
+                char c=prefix.charAt(i);
+                if(curr.children[c-'a']==null){
+                    return false;
+                }
+                curr=curr.children[c-'a'];
+            }
+            return true;
+        }
+    }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+
+/**********************PROBLEM-3**********************/
+
 }
